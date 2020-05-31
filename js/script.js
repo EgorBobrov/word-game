28d9b6da053e2fc68e19c61e92bbd2d6359797e4
EgorBobrov <egorbobrov1776@gmail.com>
2020-05-31 13:26:49 +0300

    Refactored random letter generation and alphabet use. Closes #2

--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
 const ALPHABET = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+const ONLY_LETTERS = /^[А-Яа-я]+$/;
 
 let GAME;
 
 class DefaultWordGame {
     constructor(enteredText, startLevel) {
-        this.enteredText = enteredText;
+        this.enteredText = enteredText.toLowerCase();
         this.startLevel = startLevel;
         this.level = startLevel;
         this.removedLetters = [];
+        this.alphabet = this.createAlphabet();
+        this.maxLevel = this.alphabet.length;
         this.points = 0;
         this.solutionText = this.enteredText;
     }
@@ -16,7 +19,7 @@ class DefaultWordGame {
         if (this.removedLetters.length < this.level) {
             const lettersToAdd = this.level - this.removedLetters.length;
             for(let i = 0; i < lettersToAdd; i++) {
-                this.removedLetters.push(createRandomLetter());
+                this.removedLetters.push(chooseRandomLetter(this.alphabet, this.removedLetters));
             }
             this.removedLetters.sort();
         }
@@ -27,6 +30,18 @@ class DefaultWordGame {
         for (let i = 0; i < this.removedLetters.length; i++) {
             this.solutionText = replaceSymbolInText(this.solutionText, this.removedLetters[i]);
         }
+    }
+
+    createAlphabet() {
+        let alphabet = [];
+        let text = this.enteredText.toLowerCase().split("");
+        for (let symbol of text) {
+            if (!alphabet.includes(symbol) && symbol.match(ONLY_LETTERS)) {
+                alphabet.push(symbol);
+            }
+        }
+        alphabet.sort();
+        return alphabet;
     }
 
 }
@@ -52,7 +67,7 @@ function updateTextOnScreen() {
 
 function checkSolution() {
     let solutionText = document.forms["game-content"]["game-text"].value;
-    if (solutionText.toLowerCase() === GAME.enteredText.toLowerCase() && GAME.level <= 32) {
+    if (solutionText.toLowerCase() === GAME.enteredText.toLowerCase() && GAME.level < GAME.maxLevel) {
         GAME.points += GAME.level * GAME.enteredText.length;
         alert("Отлично! У вас сейчас " + GAME.points + " очков. Переходим на уровень " + ++GAME.level);
         updateTextOnScreen();
@@ -67,12 +82,9 @@ function checkSolution() {
     }
 }
 
-function createRandomLetter() {
-    let char = ALPHABET.charAt(Math.floor(Math.random() * ALPHABET.length));
-    if(GAME.removedLetters.includes(char)) {
-        return createRandomLetter();
-    }
-    return char;
+function chooseRandomLetter(alphabet, removedLetters) {
+    let lettersToChooseFrom = alphabet.filter(letter => !removedLetters.includes(letter));
+    return lettersToChooseFrom[(Math.floor(Math.random() * lettersToChooseFrom.length))];
 }
 
 function replaceSymbolInText(text, symbol, replacement = "*") {
